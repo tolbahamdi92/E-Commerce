d7e5935a7e5e43473cd851736b343bea5707b504
tolbahamdi92 <tolba.hamdi92@gmail.com>
2023-02-23 13:55:22 +0300

    Setup Project

---
+++ E-Commerce/Shared/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  E-Commerce
+//
+//  Created by Tolba Hamdi on 2/23/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
